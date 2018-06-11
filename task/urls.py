6e7fc7bccd1c7bd789f84dd4dd25a8ddb12dfdca
Lazy-Y <zhenyanz@usc.edu>
2018-06-10 17:46:13 -0700

    remove userview

--- task/urls.py
+++ task/urls.py
@@ -8,8 +8,7 @@ from task.views import (
     TaskInvitationView,
     TaskChangeInvitationView,
     TaskRespondInvitationView,
-    TaskRevokeInvitationView,
-    UserView
+    TaskRevokeInvitationView
 )
 
 from rest_framework.schemas import get_schema_view
@@ -21,7 +20,6 @@ schema_view = get_schema_view(title='Taskit Tast API v1', url=URLS['base'])
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    url(r'^user/$', UserView.as_view()),
     url(r'^task/$', TaskListView.as_view()),
     url(r'^task/(?P<tid>[0-9a-z]*)/$', TaskDetailView.as_view()),
     url(r'^task/trigger/(?P<tid>[0-9a-z]*)/$', TaskTriggerView.as_view()),
